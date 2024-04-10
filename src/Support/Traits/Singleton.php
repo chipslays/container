df8e09994b73f3f8781cd5147a1b45f1bbc6aa9c
chipslays <gorshkov34@gmail.com>
2024-04-10 08:42:03 +0400

    feat: refactoring and add comments

--- src/Support/Traits/Singleton.php
+++ src/Support/Traits/Singleton.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Please\Container\Support\Traits;
 
 use Please\Container\Container;
@@ -7,27 +9,61 @@ use Please\Container\Exceptions\ContainerException;
 
 trait Singleton
 {
-    private static ?Container $instance = null;
+    /**
+     * The single instance of the Container.
+     *
+     * @var Container|null
+     */
+    protected static ?Container $instance = null;
 
+    /**
+     * Get the singleton instance of the Container.
+     *
+     * @return Container
+     */
     public static function getInstance(): Container
     {
-        if (self::$instance === null) {
+        if (static::canInstantiate() && self::$instance === null) {
             self::$instance = new static;
         }
 
         return self::$instance;
     }
 
-    protected function __construct() {
+    /**
+     * Prevent direct instantiation of the class.
+     */
+    protected function __construct()
+    {
         //
     }
 
-    protected function __clone() {
+    /**
+     * Prevent cloning of the singleton instance.
+     */
+    protected function __clone()
+    {
         //
     }
 
+    /**
+     * Prevent unserializing of the singleton instance.
+     *
+     * @throws ContainerException
+     */
     public function __wakeup()
     {
         throw new ContainerException('Cannot unserialize a container.');
+    }
+
+    /**
+     * Check if the class can be instantiated.
+     *
+     * @return bool
+     */
+    protected static function canInstantiate(): bool
+    {
+        $reflection = new \ReflectionClass(static::class);
+        return !$reflection->isAbstract();
     }
 }
